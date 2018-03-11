2ea351f81fd071b2cc501b15775f79358c02c3e0
Jens von Bergmann <jens@mountainmath.ca>
2018-03-11 14:47:25 -0700

    better namespacing

--- R/helpers.R
+++ R/helpers.R
@@ -5,8 +5,8 @@
 #' @export
 get_age_data <- function(dataset,regions,sum=FALSE) {
   long_labels <- function(data){
-    labels=label_vectors(data)
-    name_change=setNames(as.character(lapply(labels$Vector,function(x){return(labels %>% filter(Vector==x) %>% pull("Detail"))})),
+    labels=cancensus::label_vectors(data)
+    name_change=setNames(as.character(lapply(labels$Vector,function(x){return(labels %>% dplyr::filter(Vector==x) %>% dplyr::pull("Detail"))})),
                          labels$Vector)
     new_names=as.character(lapply(names(data),function(x){
       n=as.character(name_change[x])
@@ -16,30 +16,30 @@ get_age_data <- function(dataset,regions,sum=FALSE) {
     return(data)
   }
 
-  male <-search_census_vectors('Total - Age',dataset,"Male", quiet=TRUE) %>%
-    child_census_vectors(TRUE) %>%
-    filter(!grepl("Average",label),!grepl(" to ",label),!grepl("5 years and over",label),!grepl("Under 5 years",label))
-  female <-search_census_vectors('Total - Age',dataset,"Female", quiet=TRUE) %>%
-    child_census_vectors(TRUE) %>%
-    filter(!grepl("Average",label),!grepl(" to ",label),!grepl("5 years and over",label),!grepl("Under 5 years",label))
-  vectors <- rbind(male,female) %>% pull("vector")
-  male_data <- get_census(dataset = dataset, regions=regions,level="Regions",labels="short", vectors=male %>% pull("vector"), quiet=TRUE)
-  female_data <- get_census(dataset = dataset, regions=regions,level="Regions",labels="short", vectors=female %>% pull("vector"), quiet=TRUE)
+  male <-cancensus::search_census_vectors('Total - Age',dataset,"Male", quiet=TRUE) %>%
+    cancensus::child_census_vectors(TRUE) %>%
+    dplyr::filter(!grepl("Average",label),!grepl(" to ",label),!grepl("5 years and over",label),!grepl("Under 5 years",label))
+  female <-cancensus::search_census_vectors('Total - Age',dataset,"Female", quiet=TRUE) %>%
+    cancensus::child_census_vectors(TRUE) %>%
+    dplyr::filter(!grepl("Average",label),!grepl(" to ",label),!grepl("5 years and over",label),!grepl("Under 5 years",label))
+  vectors <- rbind(male,female) %>% dplyr::pull("vector")
+  male_data <- cancensus::get_census(dataset = dataset, regions=regions,level="Regions",labels="short", vectors=male$vector, quiet=TRUE)
+  female_data <- cancensus::get_census(dataset = dataset, regions=regions,level="Regions",labels="short", vectors=female$vector, quiet=TRUE)
 
-  labels=male_data %>% label_vectors %>% pull("Detail")
+  labels=male_data %>% cancensus::label_vectors() %>% dplyr::pull("Detail")
   labels[labels=="100 years and over"]="100+"
 
   label_levels=c("Under 1 year",seq(1,99),"100+")
 
-  data <- rbind(male_data %>% long_labels %>% mutate(Gender="Male"), female_data %>%
+  data <- rbind(male_data %>% long_labels %>% dplyr::mutate(Gender="Male"), female_data %>%
                   long_labels %>%
-                  mutate(Gender="Female")) %>%
-    mutate(`100+`=`100 years and over`) %>% select(c("Region Name","Gender",labels))
+                  dplyr::mutate(Gender="Female")) %>%
+    dplyr::mutate(`100+`=`100 years and over`) %>% dplyr::select(c("Region Name","Gender",labels))
   if (sum) {
-    data <- data %>% select(-`Region Name`) %>% group_by(Gender) %>% summarize_all(sum)
+    data <- data %>% dplyr::select(-`Region Name`) %>% dplyr::group_by(Gender) %>% summarize_all(sum)
   }
-  plot_data <- data %>% gather(key="Age", value="Population",labels) %>%
-    mutate(Age=factor(Age,levels=label_levels,ordered=TRUE))
+  plot_data <- data %>% tidyr::gather(key="Age", value="Population",labels) %>%
+    dplyr::mutate(Age=factor(Age,levels=label_levels,ordered=TRUE))
   plot_data[plot_data$Gender=="Male","Population"]=-plot_data[plot_data$Gender=="Male","Population"]
 
   return (plot_data)
@@ -81,32 +81,33 @@ common_cts <- function(data1,data2,data2_sum_vars) {
   cts_diff_2 <- setdiff(cts_2,cts_1) %>% sort
 
   d<-st_intersection(
-    data2 %>% filter(GeoUID %in% cts_diff_2) %>%
+    data2 %>% dplyr::filter(GeoUID %in% cts_diff_2) %>%
       rename(GeoUID2=GeoUID) %>%
-      select(GeoUID2) %>% mutate(area2=st_area(geometry)),
-    data1 %>% filter(GeoUID %in% cts_diff_1) %>%
-      select(GeoUID) %>% mutate(area=st_area(geometry))
+      dplyr::select(GeoUID2) %>% dplyr::mutate(area2=st_area(geometry)),
+    data1 %>% dplyr::filter(GeoUID %in% cts_diff_1) %>%
+      dplyr::select(GeoUID) %>% dplyr::mutate(area=st_area(geometry))
   )
 
-  d <- d %>% mutate(area3=st_area(geometry)) %>% mutate(ratio=as.numeric(area3/area2)) %>% filter(ratio>0.1) %>% arrange(ratio)
+  d <- d %>% dplyr::mutate(area3=st_area(geometry)) %>% dplyr::mutate(ratio=as.numeric(area3/area2)) %>% dplyr::filter(ratio>0.1) %>% arrange(ratio)
 
-  dd<- d %>% as.data.frame %>% group_by(GeoUID) %>%summarize(ratio=sum(ratio)/n(),n=n())
-  if(dd %>% filter(n<=1) %>% nrow >0) {base::stop("problem with computing common ct data")}
+  dd<- d %>% as.data.frame %>% dplyr::group_by(GeoUID) %>%summarize(ratio=sum(ratio)/n(),n=n())
+  if(dd %>% dplyr::filter(n<=1) %>% nrow >0) {base::stop("problem with computing common ct data")}
 
   ct_translation <- lapply(split(d, d$GeoUID), function(x) x$GeoUID2)
   ct_translation2 <- lapply(split(d, d$GeoUID2), function(x) x$GeoUID)
 
   new2 <- data2 %>%
-    filter(GeoUID %in% cts_diff_2) %>%
-    mutate(GeoUID2=GeoUID) %>%
-    mutate(GeoUID=as.character(ct_translation2[GeoUID2])) %>%
-    group_by(GeoUID)
+    dplyr::filter(GeoUID %in% cts_diff_2) %>%
+    dplyr::mutate(GeoUID2=GeoUID) %>%
+    dplyr::mutate(GeoUID=as.character(ct_translation2[GeoUID2])) %>%
+    dplyr::group_by(GeoUID)
 
   nnew <- summarize_at(new2,data2_sum_vars,sum)
 
-  data_2 <- rbind(data2 %>% filter(!(GeoUID %in% cts_diff_2)) %>% select("GeoUID",data2_sum_vars), nnew)
+  data_2 <- rbind(data2 %>% dplyr::filter(!(GeoUID %in% cts_diff_2)) %>% dplyr::select("GeoUID",data2_sum_vars), nnew)
   return(data_2)
 }
+
 
 #' helper for clean map theme for sf
 #' @export
@@ -114,4 +115,10 @@ map_theme <- list(
   ggplot2::theme_void(),
   ggplot2::theme(panel.grid.major = ggplot2::element_line(colour = "transparent"))
 )
+
+#' @importFrom dplyr %>%
+#' @importFrom rlang .data
+NULL
+
+
 
